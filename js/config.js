e7b1fd5da88b6f659ff73b7271350e11ec60111c
SLAMET HERRY PERMANA <153043579+HerryPermana@users.noreply.github.com>
2026-09-03 14:26:49 +0700

    Update GAS_URL to the production endpoint

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-  GAS_URL: 'GANTI_DENGAN_URL_WEB_APP_ANDA'
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbz4k1Gw7jCt_-LQuU6l3xH-swG7xMIY9CcI9dokuSKPAspab4M-pa7uTBeQ5HIeL3Y/exec'
 };
