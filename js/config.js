b043379036988395502be16a90cdd23d3c54f952
SLAMET HERRY PERMANA <153043579+HerryPermana@users.noreply.github.com>
2026-09-14 10:32:48 +0700

    Update GAS_URL in config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-  GAS_URL: 'https://script.google.com/macros/s/AKfycbwJ9bVEmO1z88CeWwUOUnKLhfJ1cKswsVdZVk_kWvfezeaP4OTy3jTWiuTvxtMeWBw/exec'
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbwM_5PE-Lis0nErg-79l97Rc3Hp59g0Obse-pS3rByq1yGmktjnrNjcsX5PGyG7HaM/exec'
 };
